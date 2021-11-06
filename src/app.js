32dcc3a568f97f5b8831f2858c57c24eeab34845
karthik2214 <maheshwaramkarthik007@gmail.com>
2021-11-06 21:09:00 +0530

    make it better

--- src/app.js
+++ src/app.js
@@ -5,15 +5,10 @@ require("./db/connection");
 const Student = require("./models/students");
 const router = new express.Router();
 const studentRouter = require("./routers/student");
-const serverless = require("serverless-http");
 
 app.use(express.json());
-app.use(`/.netlify/functions/api/`, studentRouter);
-// app.use(router);
+app.use(studentRouter);
 
 app.listen(port, () => {
   console.log(`Head Towards port ${port}`);
 });
-
-module.exports = app;
-module.exports.handler = serverless(app);
