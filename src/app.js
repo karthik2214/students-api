618319e037bd26a514745fa65254af9afe0ef33d
karthik2214 <maheshwaramkarthik007@gmail.com>
2021-11-06 20:50:09 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -5,10 +5,15 @@ require("./db/connection");
 const Student = require("./models/students");
 const router = new express.Router();
 const studentRouter = require("./routers/student");
+const serverless = require("serverless-http");
 
 app.use(express.json());
-app.use(studentRouter);
+app.use(`/.netlify/functions/api`, studentRouter);
+// app.use(router);
 
 app.listen(port, () => {
   console.log(`Head Towards port ${port}`);
 });
+
+module.exports = app;
+module.exports.handler = serverless(app);
