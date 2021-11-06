9a267546a973e115676e5191129b0eef0e2c5890
karthik2214 <maheshwaramkarthik007@gmail.com>
2021-11-06 20:09:42 +0530

    bh

--- src/app.js
+++ src/app.js
@@ -3,47 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 require("./db/connection");
 const Student = require("./models/students");
-const router = express.Router();
+const router = new express.Router();
+const studentRouter = require("./routers/student");
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello This is a API");
-});
-
-app.post("/students", async (req, res) => {
-  try {
-    const user = new Student(req.body);
-    const createUser = await user.save();
-    res.status(201).send(createUser);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.get("/students", async (req, res) => {
-  try {
-    const studentsData = await Student.find();
-    res.send(studentsData);
-  } catch (e) {
-    res.send(e);
-  }
-});
-
-app.get("/students/:id", async (req, res) => {
-  try {
-    const _id = req.params.id;
-    const studentData = await Student.findById(_id);
-
-    if (!studentData) {
-      return res.status(404).send();
-    } else {
-      res.send(studentData);
-    }
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
+app.use(studentRouter);
 
 app.listen(port, () => {
   console.log(`Head Towards port ${port}`);
---
+++ src/routers/student.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const router = new express.Router();
+const Student = require("../models/students");
+
+router.get("/", (req, res) => {
+  res.send("Hello This is a API");
+});
+
+// Create users
+router.post("/students", async (req, res) => {
+  try {
+    const user = new Student(req.body);
+    const createUser = await user.save();
+    res.status(201).send(createUser);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// Read all
+router.get("/students", async (req, res) => {
+  try {
+    const studentsData = await Student.find();
+    res.send(studentsData);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+// Read By ID
+// router.get("/students/:id", async (req, res) => {
+//   try {
+//     const _id = req.params.id;
+//     const studentData = await Student.findById(_id);
+
+//     if (!studentData) {
+//       return res.status(404).send();
+//     } else {
+//       res.send(studentData);
+//     }
+//   } catch (e) {
+//     res.status(500).send(e);
+//   }
+// });
+
+// Read By Name
+router.get("/students/:name", async (req, res) => {
+  try {
+    const name = req.params.name;
+    const studentData = await Student.find({ name });
+
+    if (!studentData) {
+      return res.status(404).send();
+    } else {
+      res.send(studentData);
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Update using ID
+// router.patch("/students/:id", async (req, res) => {
+//   try {
+//     const _id = req.params.id;
+//     const updateStudents = await Student.findByIdAndUpdate(_id, req.body, {
+//       new: true,
+//     });
+//     res.send(updateStudents);
+//   } catch (e) {
+//     res.status(400).send(e);
+//   }
+// });
+
+// Update using name
+router.patch("/students/:name", async (req, res) => {
+  try {
+    const name = req.params.name;
+    const updateStudents = await Student.findOneAndUpdate({ name }, req.body, {
+      new: true,
+    });
+    res.send(updateStudents);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// Delete using ID
+router.delete("/students/:id", async (req, res) => {
+  try {
+    const deleteStudents = await Student.findByIdAndDelete(req.params.id);
+    if (!req.params.id) {
+      return res.status(400).send();
+    }
+    res.send(deleteStudents);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+module.exports = router;
